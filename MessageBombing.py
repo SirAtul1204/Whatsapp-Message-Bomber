6a5245382e66b8bf999e1d63792f6d17f16cb65f
siratul <asatulsingh20@gmail.com>
2020-07-18 15:52:10 +0530

    Increased Performance

--- MessageBombing.py
+++ MessageBombing.py
@@ -1,30 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 import time
 
 
-msg = '''Yo!'''
+msg = '''Scammer'''
 print(msg)
 
 contact_number = input("Enter Number: ")
 StartTime = time.time()
 driver = webdriver.Chrome()
 driver.get("https://web.whatsapp.com/")
-time.sleep(10) #Wait period for you to log-in
-serviceurl = "https://wa.me/"
+time.sleep(10)  # Wait period for you to log-in
+serviceurl = "https://web.whatsapp.com/"
 params = dict()
 params['text'] = msg
-url = serviceurl + '91' + contact_number + '/?' + urllib.parse.urlencode(params)
+url = serviceurl + 'send?phone=91' + contact_number + '&' + \
+    urllib.parse.urlencode(params) + "&source&data&app_absent"
+print(url)
 driver.get(url)
 time.sleep(3)
-driver.find_element_by_xpath("//*[@id='action-button']").click()
-time.sleep(1)
-driver.find_element_by_xpath("//*[@id='fallback_block']/div/div/a").click()
-time.sleep(5)
-driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button").click()
-for i in range(0, 1000): #you can increase or decrease the number by changing 1000
-    WebElement = driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
+
+driver.find_element_by_xpath(
+    "//*[@id='main']/footer/div[1]/div[3]/button").click()
+for i in range(0, 1000):  # you can increase or decrease the number of messages by changing 1000
+    WebElement = driver.find_element_by_xpath(
+        "//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
     WebElement.send_keys(msg)
     WebElement.send_keys(Keys.ENTER)
 driver.quit()
--- README.txt
+++ README.txt
@@ -11,8 +11,7 @@ Somethings are needed to run this program correctly.
   3) Find ChromeDriver (somewhere in the bottom of page)
   4) Download correct ChromeDriver
 
-#In sourcecode (Mass Whatsapp Messenger.py) in line 18 change the [executable_path =] to the path
-where chromedriver is located
+
 
 #This is by default for Whatsapp users in India but can be changed for other countries
 You just have to change '91' in line 18 to your country's calling code
